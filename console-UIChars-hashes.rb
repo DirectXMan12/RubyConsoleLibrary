8ffd2b88503b7d32aa4614df748b6aef1d30242c
unknown <DirectXMan12@.(none)>
2009-09-05 21:12:40 -0400

    first commit - code is marginally functional

---
+++ console-UIChars-hashes.rb
@@ -0,0 +1,13 @@
+module RubyConsoleLibrary
+	class UICharacters
+		@@chars = {
+			:window => {
+				:side => "\xBA",
+				:corner_top_right => "\xBB",
+				:corner_bottom_right => "\xBC",
+				:corner_top_left => "\xC9",
+				:corner_bottom_left => "\xC8"	
+			}
+		}
+	end
+end
