8a850d02201a4072cd7db620278cd493729fad79
Solly <directxman12+github@gmail.com>
2010-04-23 10:26:19 -0700

    changed the text box control to implement adding text to the control, yet each interaction currently just takes the current text and adds an extra "x" character.  Still need to check for local overflow - maybe use a utility function in Utils, or perhaps a function in control code

--- console-controls-maincontrols.rb
+++ console-controls-maincontrols.rb
@@ -1,6 +1,20 @@
 module RubyConsoleLibrary
 	class TextBoxControl < ConsoleControl
 		
+		@current_text = "" #read, write
+
+		def current_text
+			return @current_text
+		end
+		def current_text=(t)
+			@current_text = t.to_s
+			local_cursor_x = 1
+			@current_text.each_char do |c|
+				@template[local_cursor_x][1] = c
+				local_cursor_x += 1
+			end
+		end
+
 		def initialize (parent_window, s_x,s_y)
 			super(parent_window)
 			@dims = [s_x, s_y]
@@ -30,6 +44,7 @@ module RubyConsoleLibrary
 
 		def do_interact
 			@state = :open
+			current_text = @current_text + "x"
 			return @state.to_s #success!
 		end	
 	end
